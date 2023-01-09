5074be942fe47bad60369572a4c4b018a7da12c1
Ragnar Olafsson <ragnar.olafsson@gig.com>
2023-01-09 13:42:13 +0000

    Some dart stuff that I added

--- dart_programming/dart_application/bin/dart_application.dart
+++ dart_programming/dart_application/bin/dart_application.dart
@@ -1,10 +1,25 @@
 import 'package:dart_application/dart_application.dart' as dart_application;
 
+class Person {
+  String name = 'Ragnar';
+  int age = 33;
+}
+
 double addNumbers(double num1, double num2) {
   return num1 + num2;
 }
 
 void main() {
+  // Create an instance of the Person class.
+  var p1 = Person();
+
+  // Print out the value of the class instance.
+  print(p1);
+
+  // Print out the value for Person.name and Person.age
+  print('Persons name is: ${p1.name}');
+  print('Persons age is: ${p1.age}');
+
   var firstResult = addNumbers(1, 2.5);
   print(firstResult);
 
